7c06cab3cebd918cd203bccefe9908a5486f50ed
iamchaitanyadave <chaitayadave23@gmail.com>
2020-03-23 11:56:42 +0530

    init

---
+++ ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressBar.setProgress(progress);
+                textView.setText(" "+progress+"%");
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+    }
+}
+
