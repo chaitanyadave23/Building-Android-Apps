34408be82cd509542b67d44e0bce5c3b4fa0798e
iamchaitanyadave <chaitanyadave23@gmail.com>
2020-04-23 23:58:11 +0530

    -

--- FireApp/app/src/main/java/com/example/fireapp/MainActivity.java
+++ FireApp/app/src/main/java/com/example/fireapp/MainActivity.java
@@ -2,15 +2,24 @@ package com.example.fireapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void addFirebase(View view){
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendData = (Button) findViewById(AddData);
+
+
+
     }
 }
-
 
 
