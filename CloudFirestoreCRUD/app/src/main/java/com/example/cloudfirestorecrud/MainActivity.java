25cf3b0ddcc8a26c70dd5293481abc27aba54325
iamchaitanyadave <chaitanyadave23@gmail.com>
2020-09-21 20:15:56 +0530

    Cloud Firestore CRUD Sample

--- CloudFirestoreCRUD/app/src/main/java/com/example/cloudfirestorecrud/MainActivity.java
+++ CloudFirestoreCRUD/app/src/main/java/com/example/cloudfirestorecrud/MainActivity.java
@@ -1,24 +1,14 @@
-package com.example.fireapp;
+package com.example.cloudfirestorecrud;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void addFirebase(View view){
-
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button sendData = (Button) findViewById(AddData);
-
-
-
     }
 }
-
-
